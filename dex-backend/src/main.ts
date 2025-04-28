106b2ddc5759fbb56d566e79b40b3ccc89d4d065
Lovish Badlani <lovish@dxsale.network>
2025-04-28 17:33:43 +0530

    feat: initialize dex-backend

---
+++ dex-backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(8888, '0.0.0.0');
+}
+
+bootstrap();
